f258ac33a885ed7d1e90ac69ee8527b8bc47d29d
karim2251 <elalaouiabdelkarim22@gmail.com>
2024-07-01 16:09:58 +0100

    add skills

--- src/components/Topics.vue
+++ src/components/Topics.vue
@@ -1,6 +1,6 @@
 <template>
   
-  <div v-for="d in data" :key="d" class="card">
+  <div v-for="d in data" :key="d" class="card" data-aos="zoom-out-right" :data-aos-delay="d.cpt">
     <span class="icon"><img :src="d.icon" :alt="d.name"></span>
     <span class="title">{{d.name}}</span>
   </div>
